07e03b24fb5ee3ec3f9fcd1bdbaa03acd388977f
CristianRC03 <crizcamach2003@gmail.com>
2024-05-14 15:14:31 -0600

    Puesh

--- examen/src/app/views/detalles-candidato/detalles-candidato.component.ts
+++ examen/src/app/views/detalles-candidato/detalles-candidato.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { CandidatoController } from 'src/app/controllers/CandidatoController';
 
 
 @Component({
@@ -7,10 +8,15 @@ import { ModalController } from '@ionic/angular';
   templateUrl: './detalles-candidato.component.html',
   styleUrls: ['./detalles-candidato.component.scss'],
 })
-export class DetallesCandidatoComponent {
+export class DetallesCandidatoComponent implements OnInit{
   @Input() candidato: any;
+  
 
   constructor(private modalController: ModalController) {}
+
+  ngOnInit() {
+    console.log('Candidato:', this.candidato);
+  }
 
   dismiss() {
     this.modalController.dismiss();
